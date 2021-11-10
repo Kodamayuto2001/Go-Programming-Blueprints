6892d442c40b45692d44c6b40ef7798b37defd03
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-11-10 23:41:48 +0900

    追加

--- Chapter1/1-3/main.go
+++ Chapter1/1-3/main.go
@@ -6,6 +6,7 @@ import (
 	"text/template"
 	"path/filepath"
 	"sync"
+	"flag"
 )
 
 type templateHandler struct {
@@ -19,17 +20,32 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(
 			template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
+// func main() {
+// 	r := newRoom()
+// 	http.Handle("/", &templateHandler{filename: "chat.html"})
+// 	http.Handle("/room", r)
+// 	//	チャットルームを開始します。
+// 	go r.run()
+// 	//	Webサーバーを起動します。
+// 	if err := http.ListenAndServe(":8080", nil); err != nil {
+// 		log.Fatal("ListenAndServe:", err)
+// 	}
+// }
+
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()	//	フラグを解釈します
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	//	チャットルームを開始します。
 	go r.run()
-	//	Webサーバーを起動します。
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	//	Webサーバーを起動します
+	log.Println("Webサーバーを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
