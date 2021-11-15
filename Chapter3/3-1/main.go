7a2c7b058e2bb3909cfb0017612a67cd815812c4
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-11-15 22:46:45 +0900

    追加

---
+++ Chapter3/3-1/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(
+			template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
+}
+
+func randomString() string {
+	capacity := 40
+	slice := make([]byte, capacity)
+	rand.Read(slice)
+	return base64.URLEncoding.EncodeToString(slice)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	GOOGLE_CLIENT_ID := os.Getenv("GOOGLE_CLIENT_ID")
+	GOOGLE_CLIENT_SECRET := os.Getenv("GOOGLE_CLIENT_SECRET")
+	// FACEBOOK_CLIENT_ID := os.Getenv("FACEBOOK_CLIENT_ID")
+	// FACEBOOK_CLIENT_SECRET := os.Getenv("FACEBOOK_CLIENT_SECRET")
+
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() //	フラグを解釈します
+
+	//	Gomniauthのセットアップ
+	gomniauth.SetSecurityKey(randomString())
+	gomniauth.WithProviders(
+		google.New(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "http://localhost:8080/auth/callback/google"),
+	)
+
+	r := newRoom()
+	// r.tracer = trace.New(os.Stdout)
+
+	// http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		//	Value:	クッキーを削除しないブラウザもあるため、Valueの値（ユーザーについてのデータが格納されていた）を空文字列で上書きしている。
+		//	MaxAge:	クッキーのMaxAgeの値を-1と指定することで、ブラウザ上のクッキーは即座に削除される。
+		http.SetCookie(w, &http.Cookie{
+			Name:	"auth",
+			Value:	"",
+			Path:	"/",
+			MaxAge:	-1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
+	//	チャットルームを開始します。
+	go r.run()
+
+	//	Webサーバーを起動します
+	log.Println("Webサーバーを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ Chapter3/3-1/message.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"time"
+)
+
+//	messageは一つのメッセージを表します。
+//	Name:		ユーザー名
+//	Message:	メッセージ
+//	When:		メッセージが送信された時刻
+//	AvatarURL	アバターのURLを保持できるようにする
+type message struct {
+	Name    	string
+	Message 	string
+	When    	time.Time
+	AvatarURL 	string
+}
