e06b45cb4ac0e9a351db8cadc7b0f63db4a7ac7e
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-11-21 20:35:46 +0900

    追加

---
+++ Chapter4-domain-search-command-line-tools/src/sprinkle/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+
+	// "strings"
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+const otherWord = "*"
+
+var transforms = []string{
+	otherWord,
+	otherWord,
+	otherWord,
+	otherWord,
+	otherWord + "app",
+	otherWord + "site",
+	otherWord + "time",
+	"get" + otherWord,
+	"go" + otherWord,
+	"lets " + otherWord,
+}
+
+type Sprinkle struct {
+	ID		int
+	Prefix	string
+	Suffix 	string
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@/"+os.Getenv("DB_NAME"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	
+	// for s.Scan() {
+	// 	t := transforms[rand.Intn(len(transforms))]
+	// 	fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+	// }
+
+	// var idList []interface{}
+	// err = db.QueryRow("select id from sprinkle").Scan(&idList)
+	// fmt.Println(idList)
+	// fmt.Println(len(idList))
+	// // id := rand.Intn(len(idList))
+	// id := 5
+
+	// var (
+	// 	prefix string
+	// 	suffix string
+	// )
+	// err = db.QueryRow("select prefix, suffix from sprinkle where id = ?", id).Scan(&prefix, &suffix)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(prefix)
+	// fmt.Println(suffix)
+	// fmt.Println(s)
+
+	// rows, err := db.Query("select * from sprinkle")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer rows.Close()
+
+	// for rows.Next() {
+	// 	var sprinkle Sprinkle
+	// 	err := rows.Scan(&sprinkle.ID, &sprinkle.Prefix, &sprinkle.Suffix)
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	fmt.Println(sprinkle.ID, sprinkle.Prefix, sprinkle.Suffix)
+	// }
+
+	// fmt.Println("ok")
+	// fmt.Println(s)
+
+	rows, err := db.Query("select count(*) from sprinkle")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var count int 
+
+	for rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Printf("Number of rows are %d\n", count)
+	fmt.Println(s)
+}
